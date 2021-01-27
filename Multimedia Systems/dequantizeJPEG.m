ab51b09d6bfdd45a98e1525e052b921237dcfd86
Kostantinos Giokotos <64098294+kostasgioko@users.noreply.github.com>
2021-01-27 15:31:27 +0200

    Delete Multimedia Systems directory

--- Multimedia Systems/dequantizeJPEG.m
+++
@@ -1,7 +0,0 @@
-function dctBlock = dequantizeJPEG(qBlock, qTable, qScale)
-    %This function dequantizes quantized DCT block of the image.    
-    
-    %Multiply each element of the block with the corresponding quantization value, and then with the quantization scale.
-    dctBlock = qScale .* qTable .* qBlock;
-    
-end
--- Multimedia Systems/quantizeJPEG.m
+++
@@ -1,7 +0,0 @@
-function qBlock = quantizeJPEG(dctBlock, qTable, qScale)
-    %This function quantizes a DCT block of the image.
-    
-    %Divide each element of the block with the corresponding quantization value, then with the quantization scale and round the result.
-    qBlock = round(dctBlock ./ qTable ./ qScale);
-    
-end
